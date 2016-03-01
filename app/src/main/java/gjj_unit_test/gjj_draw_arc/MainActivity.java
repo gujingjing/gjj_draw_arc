2b1a4736434ff61a5f6a550d87deeaf87346af62
gujngjing <1546804319@qq.com>
2016-03-01 11:44:14 +0800

    画钟表图片

---
+++ app/src/main/java/gjj_unit_test/gjj_draw_arc/MainActivity.java
@@ -0,0 +1,20 @@
+package gjj_unit_test.gjj_draw_arc;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
